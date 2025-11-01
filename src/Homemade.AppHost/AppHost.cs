70b42dabd29c1859aa97856a794138cf8a585c23
Wannes Gennar <wannes.gennar@gmail.com>
2025-11-01 11:55:12 +0100

    chore: add Blazor project

--- src/Homemade.AppHost/AppHost.cs
+++ src/Homemade.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddPostgres("postgres")
@@ -12,5 +14,8 @@ builder.AddOllama("ollama")
 builder.AddGarnet("cache");
 
 builder.AddMailPit("mailpit");
+
+builder.AddProject<Homemade_Web>("web")
+    .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
