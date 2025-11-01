4c8d69d7fff72c4cd10bcb435fe9c973915dc31e
Wannes Gennar <wannes.gennar@gmail.com>
2025-11-01 11:52:13 +0100

    chore: add services

--- src/Homemade.AppHost/AppHost.cs
+++ src/Homemade.AppHost/AppHost.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddPostgres("postgres")
+    .AddDatabase("recipes");
+
+builder.AddKeycloak("keycloak");
+    // .WithRealmImport("../../keycloak/realms");
+
+builder.AddOllama("ollama")
+    .AddModel("gemma3:1b");
+
+builder.AddGarnet("cache");
+
+builder.AddMailPit("mailpit");
+
 builder.Build().Run();
